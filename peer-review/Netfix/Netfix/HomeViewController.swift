f7a96a721b7a98298856a167af2e42d7b67a225f
Craig Martin <craigmart86@gmail.com>
2025-05-12 16:24:23 +1000

    Project Setup

---
+++ peer-review/Netfix/Netfix/HomeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Netfix
+//
+//  Created by Craig Martin on 12/5/2025.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
