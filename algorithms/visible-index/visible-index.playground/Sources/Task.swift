e54689633ecf581d691cf509b3283ea88a1d1340
Khai Liew <khai.liew07@gmail.com>
2025-05-12 18:00:48 +1000

    VisibleIndices solution

--- algorithms/visible-index/visible-index.playground/Sources/Task.swift
+++ algorithms/visible-index/visible-index.playground/Sources/Task.swift
@@ -3,6 +3,22 @@ public struct Item {
 }
 
 public func visibleIndices(items: [Item], scrollOffset: Int, viewportWidth: Int) -> [Int] {
-    // TODO: Implement the function to calculate visible indices
-    return []
+    // Solution
+    var result: [Int] = []
+    var currentOffset = 0
+
+    for (index, item) in items.enumerated() {
+        let itemStart = currentOffset
+        let itemEnd = currentOffset + item.width
+        let viewportStart = scrollOffset
+        let viewportEnd = scrollOffset + viewportWidth
+
+        if itemEnd > viewportStart && itemStart < viewportEnd {
+            result.append(index)
+        }
+
+        currentOffset += item.width
+    }
+
+    return result
 }
